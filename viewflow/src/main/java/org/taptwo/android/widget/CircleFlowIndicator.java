b375f85c9ce2b374941a5931dcdb461fc70ae96a
Dries De Smet <dries@inthepocket.mobi>
2014-09-22 10:17:27 +0200

    Update lib with new Android build system

--- viewflow/src/main/java/org/taptwo/android/widget/CircleFlowIndicator.java
+++ viewflow/src/main/java/org/taptwo/android/widget/CircleFlowIndicator.java
@@ -26,6 +26,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
+
 import org.taptwo.android.widget.viewflow.R;
 
 /**
